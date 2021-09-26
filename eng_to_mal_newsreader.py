25e9fd48e9836015942de2cd193fcc6baa6016e6
Sabarinath E M <76653561+sabarinathem@users.noreply.github.com>
2021-09-27 00:17:13 +0530

    Update eng_to_mal_newsreader.py

--- eng_to_mal_newsreader.py
+++ eng_to_mal_newsreader.py
@@ -7,16 +7,16 @@ from googletrans import Translator
 
 # Read the English text from Image
 img=Image.open('malayalam.png')
-text=pyt.image_to_string(img,lang='mal')
+text=pyt.image_to_string(img)
 text=text.replace('\n',' ')
 
 # Translate the extrated english text to malayalam
 transl=Translator()
-out=transl.translate(text,dest='en')
+out=transl.translate(text,dest='ml')
 mal_text=out.text
 print(mal_text)
 
 # read the computer by its own voice
-text=gTTS(mal_text,lang='en',slow=False)
+text=gTTS(mal_text,lang='ml',slow=False)
 text.save("voice.mp3")
 playsound("voice.mp3")
