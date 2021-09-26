cf23bcc7296e44388151ec4cdb38e199106ee4c0
Sabarinath E M <76653561+sabarinathem@users.noreply.github.com>
2021-09-27 00:15:52 +0530

    Add files via upload

--- eng_to_mal_newsreader.py
+++ eng_to_mal_newsreader.py
@@ -6,15 +6,17 @@ from translate import Translator
 from googletrans import Translator
 
 # Read the English text from Image
-img=Image.open('story_eng.png')
-text=pyt.image_to_string(img)
+img=Image.open('malayalam.png')
+text=pyt.image_to_string(img,lang='mal')
 text=text.replace('\n',' ')
+
 # Translate the extrated english text to malayalam
 transl=Translator()
-out=transl.translate(text,dest='ml')
+out=transl.translate(text,dest='en')
 mal_text=out.text
+print(mal_text)
 
 # read the computer by its own voice
-text=gTTS(mal_text,lang='ml')
+text=gTTS(mal_text,lang='en',slow=False)
 text.save("voice.mp3")
 playsound("voice.mp3")
